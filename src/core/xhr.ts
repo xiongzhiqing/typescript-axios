91a32ae7acffbe936e7faf03c5580b5294c66be1
xiongzhiqing <xiongzhiqing@everjiankang.com>
2020-07-13 17:00:17 +0800

    test(interceptor.spec.ts file): interceptor.spec.ts 响应请求拦截器测试
    interceptor.spec.ts 响应请求拦截器测试

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -11,7 +11,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       url,
       method = 'get',
       data = null,
-      headers,
+      headers = {},
       responseType,
       timeout,
       cancelToken,
